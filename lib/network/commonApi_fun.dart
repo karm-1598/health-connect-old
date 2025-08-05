57f35cd4155f93bf56713dd44ce06171717a40a3
Karm Patel <karmp8690@gmail.com>
2025-08-05 11:42:16 +0530

    optimised the code

--- lib/network/commonApi_fun.dart
+++ lib/network/commonApi_fun.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 class baseApi{
   static Dio _dio=Dio(BaseOptions(
-    baseUrl: 'http://192.168.1.7/api/',
+    baseUrl: 'http://192.168.196.215/api/',
     connectTimeout: Duration(seconds: 10),
     receiveTimeout: Duration(seconds: 10),
     headers: {"Content-Type": "application/json"},
